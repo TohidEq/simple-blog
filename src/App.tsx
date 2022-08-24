148bb1e14627b6ef976d045cf38cb7545592dd41
TohidEq <TohidEq@gmail.com>
2022-08-24 12:42:26 +0430

    Move project from JS to TS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='bg-darck-c-2 text-white-c'>
+        <h2>Navigation</h2>
+        
+        
+        
+        
+        
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'darck-c-2': "#343434",
+      'darck-c-1' : "#8E8B82",
+      'white-c': "#F3F3F3",
+      'dead-c': "#DCDCD9",
+      'hover-c': "#E9DCBE"
+
+
+    },
+  },
+  plugins: [],
+}
