a5f141758a99dc3ec9e0791be1c43be30540ae09
TohidEq <TohidEq@gmail.com>
2022-08-27 01:00:19 +0430

    Basic Navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,8 +5,17 @@ export interface IHomeProps {
 
 export default function Home (props: IHomeProps) {
   return (
-    <div>
+    <div className='mx-auto container'>
       home
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     "./src/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
     colors: {
       'darck-c-2': "#343434",
       'darck-c-1' : "#8E8B82",
