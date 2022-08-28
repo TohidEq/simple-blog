aaddecd5b7a64188ce42a3e4d388af6c83a8af41
TohidEq <TohidEq@gmail.com>
2022-08-28 15:25:47 +0430

    Complete Home + Cards

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,18 +5,190 @@ export interface IHomeProps {
 
 export default function Home (props: IHomeProps) {
   return (
-    <div className='mx-auto container'>
-      home
-      
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione in voluptas quod beatae, non optio? Impedit, nihil voluptatibus. Quis velit aliquam eveniet iure explicabo illo cumque officiis autem at!</p>
+    <div className='home'>
+      <h1>Home</h1>
+      <div className="home-cards">
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+
+        <a href='#5' className="home-card">
+          <div className="card-title">Title</div>
+          <div className="card-texts">
+            <div className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
+            </div>
+            <div className="card-info">
+              <div className="card-author">
+                By <a href="#2"> TohidEq</a>
+              </div>
+              <div className="card-date">22 May 2022</div>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
+
   );
 }
