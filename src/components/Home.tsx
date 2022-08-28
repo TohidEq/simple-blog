ea7c31cc14b685723733ca9f7906efe36199c3da
TohidEq <TohidEq@gmail.com>
2022-08-28 15:55:19 +0430

    Fix problem (Cards hover)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ export default function Home (props: IHomeProps) {
     <div className='home'>
       <h1>Home</h1>
       <div className="home-cards">
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -23,7 +23,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -38,7 +38,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -53,7 +53,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -68,7 +68,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -83,7 +83,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -98,7 +98,7 @@ export default function Home (props: IHomeProps) {
           </div>
         </a>
 
-        <a href='#5' className="home-card">
+        <a href='#5' className="home-card group">
           <div className="card-title">Title</div>
           <div className="card-texts">
             <div className="card-text">
@@ -112,81 +112,7 @@ export default function Home (props: IHomeProps) {
             </div>
           </div>
         </a>
-
-        <a href='#5' className="home-card">
-          <div className="card-title">Title</div>
-          <div className="card-texts">
-            <div className="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
-            </div>
-            <div className="card-info">
-              <div className="card-author">
-                By <a href="#2"> TohidEq</a>
-              </div>
-              <div className="card-date">22 May 2022</div>
-            </div>
-          </div>
-        </a>
-
-        <a href='#5' className="home-card">
-          <div className="card-title">Title</div>
-          <div className="card-texts">
-            <div className="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
-            </div>
-            <div className="card-info">
-              <div className="card-author">
-                By <a href="#2"> TohidEq</a>
-              </div>
-              <div className="card-date">22 May 2022</div>
-            </div>
-          </div>
-        </a>
-
-        <a href='#5' className="home-card">
-          <div className="card-title">Title</div>
-          <div className="card-texts">
-            <div className="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
-            </div>
-            <div className="card-info">
-              <div className="card-author">
-                By <a href="#2"> TohidEq</a>
-              </div>
-              <div className="card-date">22 May 2022</div>
-            </div>
-          </div>
-        </a>
-
-        <a href='#5' className="home-card">
-          <div className="card-title">Title</div>
-          <div className="card-texts">
-            <div className="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
-            </div>
-            <div className="card-info">
-              <div className="card-author">
-                By <a href="#2"> TohidEq</a>
-              </div>
-              <div className="card-date">22 May 2022</div>
-            </div>
-          </div>
-        </a>
-
-        <a href='#5' className="home-card">
-          <div className="card-title">Title</div>
-          <div className="card-texts">
-            <div className="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam velit beatae, et enim ab itaque earum nulla nihil iure....
-            </div>
-            <div className="card-info">
-              <div className="card-author">
-                By <a href="#2"> TohidEq</a>
-              </div>
-              <div className="card-date">22 May 2022</div>
-            </div>
-          </div>
-        </a>
+        
       </div>
     </div>
 
