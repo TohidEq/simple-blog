10194807992276df83b85ce5d7944e27583571ca
TohidEq <TohidEq@gmail.com>
2022-08-31 16:01:02 +0430

    Design Create page

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -3,10 +3,47 @@ import * as React from 'react';
 export interface ICreateProps {
 }
 
+
+
+
 export default function Create (props: ICreateProps) {
   return (
-    <div>
-        create
-    </div>
+    <div className="create">
+      <h1>Create New blog</h1>
+        <form >
+
+          <div className="title-box">
+            <label > Title</label>
+            <input className='input'
+                type="text"
+                required
+            />
+          </div>
+
+          <div className="body-box">
+            <label >Preview text</label>
+            <textarea className='input preview-input'
+                required
+                
+            ></textarea>  
+          </div>
+          
+
+          <div className="body-box">
+            <label >Body</label>
+            <textarea className='input'
+                required
+                
+            ></textarea>  
+          </div>
+          
+
+
+          <button >Add Blog</button>
+
+
+
+        </form>
+    </div>  
   );
 }
